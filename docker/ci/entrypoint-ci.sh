182659e6242acee90c4599b1efe4b18f32fd8ece
Rafael Reimberg <rafael.reimberg@mercadobitcoin.com.br>
2018-03-30 11:44:15 -0300

    Add redis to docker-compose

--- docker/ci/entrypoint-ci.sh
+++ docker/ci/entrypoint-ci.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+/bin/bash
+
+# Application execution
+echo "Checking DB service availability..."
+while true; do
+    ANSWER=`nmap -sS -O mysql.docker -p 3306 | egrep '^'3306'/tcp' |egrep -v 'close'| wc -c`
+    [ $ANSWER -gt 0 ] &&{
+        break
+    }
+    sleep 1
+    echo "."
+done
+
+echo "Starting development server"
+
 cd /source/ && pytest -vv tests/integration
